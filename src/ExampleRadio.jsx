37d1a06f96e6b7983477373a6ce43e9afe83377c
YM051198 <ym051198@cerner.com>
2018-08-30 15:08:55 +0530

    My Last day commit

--- src/ExampleRadio.jsx
+++ src/ExampleRadio.jsx
@@ -4,18 +4,18 @@ import React from 'react';
 const ExampleRadio = () =>{
   return(
     <fieldset>
-    <label>
-      <input type="checkbox" class="Beige" value="Red" />
-      Click me
-    </label>
-    <label>
-      <input type="checkbox" class="Beige" value="Blue"/>
-      Click me
-    </label>
-    <label>
-      <input type="checkbox" class="Beige" value="Green"/>
-      Click me
-    </label>
+      <label>
+        <input type="checkbox" class="Colors" value="Red" />
+        Click me
+      </label>
+      <label>
+        <input type="checkbox" class="Colors" value="Blue"/>
+        Click me
+      </label>
+      <label>
+        <input type="checkbox" class="Colors" value="Green"/>
+        Click me
+      </label>
   </fieldset>
   );
 }
